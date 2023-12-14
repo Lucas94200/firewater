adb8540190a0c12b1fd76c60e15aaa040eeaf6ac
Lucas Yao <114393993+Lucas94200@users.noreply.github.com>
2023-12-14 15:15:36 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app'
 
 const app = initializeApp({
-  apiKey: "", // TODO
-  authDomain: "", // TODO
-  databaseURL: "", // TODO
-  projectId: "", // TODO
-  storageBucket: "" // TODO
+  apiKey: "AIzaSyCyw_mDZuWOLYIfykQdw0Vglx0ZZMd_vDs", // TODO
+  authDomain: "firewater-d2a1e.firebaseapp.com", // TODO
+ // databaseURL: "", // TODO
+  projectId: "firewater-d2a1e", // TODO
+ // storageBucket: "" // TODO
 })
 
 export default app
